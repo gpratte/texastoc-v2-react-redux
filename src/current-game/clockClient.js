f65cae25a3c27ffc9c792ce0abdf4f5d5676f6e9
Gil Pratte <gil.pratte@gmail.com>
2020-04-12 13:03:57 -0500

    resume, pause, forward and backup the clock

--- src/current-game/clockClient.js
+++ src/current-game/clockClient.js
@@ -19,3 +19,79 @@ export function getClock(callback) {
       leagueStore.dispatch({type: API_ERROR, message: message})
     });
 }
+
+export function resume() {
+  const gameId = leagueStore.getState().game.data.id;
+  const token = leagueStore.getState().token.token;
+
+  API.post('/api/v2/games/' + gameId + '/clock', {}, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/vnd.texastoc.clock-resume+json'
+    }
+  })
+    .then(result => {
+      // do nothing
+    })
+    .catch(function (error) {
+      const message = error.message ? error.message : error.toString();
+      leagueStore.dispatch({type: API_ERROR, message: message})
+    });
+}
+
+export function pause() {
+  const gameId = leagueStore.getState().game.data.id;
+  const token = leagueStore.getState().token.token;
+
+  API.post('/api/v2/games/' + gameId + '/clock', {}, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/vnd.texastoc.clock-pause+json'
+    }
+  })
+    .then(result => {
+      // do nothing
+    })
+    .catch(function (error) {
+      const message = error.message ? error.message : error.toString();
+      leagueStore.dispatch({type: API_ERROR, message: message})
+    });
+}
+
+export function back() {
+  const gameId = leagueStore.getState().game.data.id;
+  const token = leagueStore.getState().token.token;
+
+  API.post('/api/v2/games/' + gameId + '/clock', {}, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/vnd.texastoc.clock-back+json'
+    }
+  })
+    .then(result => {
+      // do nothing
+    })
+    .catch(function (error) {
+      const message = error.message ? error.message : error.toString();
+      leagueStore.dispatch({type: API_ERROR, message: message})
+    });
+}
+
+export function forward() {
+  const gameId = leagueStore.getState().game.data.id;
+  const token = leagueStore.getState().token.token;
+
+  API.post('/api/v2/games/' + gameId + '/clock', {}, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/vnd.texastoc.clock-forward+json'
+    }
+  })
+    .then(result => {
+      // do nothing
+    })
+    .catch(function (error) {
+      const message = error.message ? error.message : error.toString();
+      leagueStore.dispatch({type: API_ERROR, message: message})
+    });
+}
--- src/current-game/components/Clock.jsx
+++ src/current-game/components/Clock.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {getClock} from '../clockClient'
+import Button from "react-bootstrap/Button";
+import {getClock, resume, pause, back, forward} from '../clockClient'
 
 /*
  *
@@ -28,12 +29,61 @@ class Clock extends React.Component {
   }
 
   render() {
+    const clock = this.state.clock;
+    if (!clock) return null;
+
+    let seconds = '' + clock.seconds;
+    if (clock.seconds < 10) {
+      seconds = seconds.padStart(2, '0');
+    }
     return (
       <div>
-        <h1>Clock</h1>
         {
-          this.state.clock &&
-          <span>{JSON.stringify(this.state.clock)}</span>
+          clock &&
+          <div>
+            <span>{clock.thisRound.name}</span>
+            &nbsp;&nbsp;&nbsp;
+            <span>{clock.minutes}</span>:<span>{seconds}</span>
+            &nbsp;&nbsp;&nbsp;
+            <span>{clock.thisRound.bigBlind}</span>/
+            <span>{clock.thisRound.smallBlind}</span>/
+            <span>{clock.thisRound.ante}</span>
+            <br/>
+            {
+              !clock.playing &&
+              <Button variant="link"
+                      onClick={() => back()}>
+                <i className="fas fa-step-backward"></i>
+              </Button>
+            }
+            {
+              clock.playing &&
+              <Button variant="link"
+                      onClick={() => pause()}>
+                <i className="fas fa-pause"></i>
+              </Button>
+            }
+            {
+              !clock.playing &&
+              <Button variant="link"
+                      onClick={() => resume()}>
+                <i className="fas fa-play"></i>
+              </Button>
+            }
+            {
+              !clock.playing &&
+              <Button variant="link"
+                      onClick={() => forward()}>
+                <i className="fas fa-step-forward"></i>
+              </Button>
+            }
+            <br/>
+            <span>{clock.nextRound.name}</span>
+            &nbsp;&nbsp;&nbsp;
+            <span>{clock.nextRound.bigBlind}</span>/
+            <span>{clock.nextRound.smallBlind}</span>/
+            <span>{clock.nextRound.ante}</span>
+          </div>
         }
       </div>
     );
