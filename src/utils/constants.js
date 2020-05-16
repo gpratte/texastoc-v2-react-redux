82c3dba13d6efda4f06447019cbd419ab6e592f0
Gil Pratte <gil.pratte@gmail.com>
2020-05-16 12:36:38 -0500

    constants for running in different environments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,14 @@
 
 export const VERSION = "2.1";
+
+// Local non-tomcat
+// export const CLIENT_URL = "http://localhost:3000";
+// export const SERVER_URL = "http://localhost:8080";
+
+// Local tomcat
+// export const CLIENT_URL = "http://localhost:8080";
+// export const SERVER_URL = "http://localhost:8080/server";
+
+// Prod
 export const CLIENT_URL = "https://texastoc.com";
 export const SERVER_URL = "https://texastoc.com/server";
